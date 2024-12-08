f89b7838d2fb0c5c21f7fd75c200abe32740d2b5
aadheil <adilkkinbox@gmail.com>
2024-12-08 14:05:35 +0530

    super admin dashboard route added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import Home from './Home/Home'
 import Api from './Home/Api'
+import DashboardSA from './Super-admin/DashboardSA'
 
 function App() {
   return (
-    <Home/>
-    // <Api/>
+   
+    <Routes>
+    <Route path='/' element={<Home/>} />
+    <Route path='/super-admin/dashboard' element={<DashboardSA />} />
+    </Routes>
   )
 }
 
