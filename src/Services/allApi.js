428119df13cb4b463ebc9eeca627d2794818202b
Iamdkj@237 <dhanesh4u237@gmail.com>
2024-12-10 21:15:59 +1000

    get restaurants

--- src/Services/allApi.js
+++ src/Services/allApi.js
@@ -3,6 +3,9 @@ import { commonAPI } from "./commonApi";
  export const GetProductList = async(headers)=>{
     return await commonAPI("GET",`/api/v1/menus`,"",headers)
  }
+ export const GetRestaurantList = async(headers)=>{
+   return await commonAPI("GET",`/api/v1/restaurants`,"",headers)
+}
  export const AddProduct = async(body,headers)=>{
    return await commonAPI("POST",`/api/v1/menus`,body,headers)
 }
