ad939ccba5973ac40e0ea1ea3272d3c338036c27
aadheil <adilkkinbox@gmail.com>
2024-12-14 15:43:13 +0530

    query params added

--- src/Services/allApi.js
+++ src/Services/allApi.js
@@ -1,7 +1,7 @@
 import { commonAPI } from "./commonApi";
 
  export const getMenuList = async(headers) => {
-    return await commonAPI("GET",`/api/v1/menus`,"",headers)
+    return await commonAPI("GET",`/api/v1/menus?restaurantId=REST-a4127b25-cb33-4969-aa88-128509849405&restaurantName=aryas`,"",headers)
  }
  
  export const getRestaurantList = async(headers) => {
